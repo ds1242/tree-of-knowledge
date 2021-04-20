083e486c7448a46406ef18757926d289f98ea70b
dshaw1242 <david.shaw1242@gmail.com>
2021-04-20 07:47:31 -0600

    issue 3: psuedocoded results from search

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,14 +13,19 @@ function bookSearch (searchTerm){
             var pageCount = data.items[i].volumeInfo.pageCount;
             var publishDate = data.items[i].volumeInfo.publishedDate;
             var title = data.items[i].volumeInfo.title;
-            var subtitle = data.items[i].volumeInfo.subtitle;
-
-            // need function here to display information 
-            // function displayBookInfo(author, pageCount, publishDate, title, subtitle)
-
-        }
-        
-
+            var subtitle = data.items[i].volumeInfo.subtitle;            
+            //function to display information 
+            displayBookInfo(author, pageCount, publishDate, title, subtitle)
+        }        
     })
 }
-bookSearch();
+
+function displayBookInfo(author, pageCount, publishDate, title, subtitle){
+    // create display card(foundation so possibly something else)
+    // append to found books
+    // button to save to wishlist with local storage
+    // save to recent search page (do we only want a few results displayed?)
+    console.log(title, subtitle, author, pageCount,publishDate);
+}
+
+bookSearch('intitle:Hobbit');
